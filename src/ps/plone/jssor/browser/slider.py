62bd315f68e1334c02439a573dc4d51b9305051a
Jens Krause <jens@propertyshelf.com>
2015-07-14 17:33:56 +0700

    change default setting for caption data layouts

--- src/ps/plone/jssor/browser/slider.py
+++ src/ps/plone/jssor/browser/slider.py
@@ -634,7 +634,7 @@ class SliderViewlet(ViewletBase):
 
     @property
     def CaptionDataLayout(self):
-        return self.Settings.get('CS_DataType', 'location_object-type_price')
+        return self.Settings.get('CS_DataType', 'location_property-type_info_price')
 
     @property
     def isLongCaption(self):
@@ -1424,7 +1424,7 @@ class ICaptionSlider(Interface):
     )
 
     CS_DataType = schema.Choice(
-        default=u'title_location_info_price',
+        default=u'location_property-type_info_price',
         required=True,
         title=_(
             u"label_CS_DataType",
